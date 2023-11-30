fdf957f6ff3b3da672f6297e39a32d3e4bc83e9d
GuidoRM <guidorafaelmamani100@gmail.com>
2023-11-30 10:41:21 -0300

    fix: bug in Image

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { lusitana } from './ui/fonts';
+import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
 export default function Page() {
@@ -36,16 +36,15 @@ export default function Page() {
             width={1000}
             height={760}
             className="hidden md:block"
-            alt={"Screenshots of the dashboard project showing desktop version"}
+            alt="Screenshots of the dashboard project showing desktop version"
           />
-
           <Image
             src="/hero-mobile.png"
             width={560}
             height={620}
             className="block md:hidden"
-            alt={"Screenshot of the dashboard project showing mobile version"}
-            />
+            alt="Screenshot of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
